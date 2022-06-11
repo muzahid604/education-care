889394a2a16a0f725ed0123c83b7bc85f6a16ebf
muzahid604 <604muzahid@gmail.com>
2022-06-11 06:15:08 +0600

    first commit

---
+++ src/firebase.init.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import getAuth from 'firebase/auth'
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCtceKVz0uzKdg-pq4oLP-zDuhc-fOAU6M",
+    authDomain: "education-care-7769b.firebaseapp.com",
+    projectId: "education-care-7769b",
+    storageBucket: "education-care-7769b.appspot.com",
+    messagingSenderId: "999009023961",
+    appId: "1:999009023961:web:539ae5a17f681991690bc8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export default auth;
